b9daa96b353db3128406a59e354c5188042a25b9
Tan Dung <nguyentandung9111@gmail.com>
2024-02-29 14:26:49 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,18 @@ import 'package:flutter/material.dart';
 // Bước 2: Flutter sẽ chạy hàm main này đầu tiên
 void main() {
   // Bước 3: Sẽ chạy cái này để khởi động app
-  runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false,
+  runApp(MaterialApp(
       // Bước 4: Sử dụng thành phần để xây dựng giao diện người dùng
-      home: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: Text(
-          'Hello 8World 2341233',
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+              backgroundColor: Colors.red, title: const Text('Tự học flutter')),
+          body: const Center(
+            child: Text(
+              'Hello 8World 2341233',
+            ),
+          ),
         ),
-      )));
+      ),
+      debugShowCheckedModeBanner: false));
 }
